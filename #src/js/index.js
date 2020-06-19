8dd8b4245accca74df4d9b3a04bf62961c93d653
aeroman256 <zakharov.dmitry.92@yandex.ru>
2020-06-20 01:24:49 +0300

    header complete

--- #src/js/index.js
+++ #src/js/index.js
@@ -1,1 +1,6 @@
-@@include("alert.js")
+
+let user_icon = document.querySelector(".user-header__icon")
+user_icon.addEventListener("click", (e) => {
+  let user_menu = document.querySelector(".user-header__menu")
+  user_menu.classList.toggle("_active")
+})
