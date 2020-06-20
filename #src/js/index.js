ea164153224c40955f21176924bae6e08c89a298
aeroman256 <zakharov.dmitry.92@yandex.ru>
2020-06-20 15:43:39 +0300

    add burger

--- #src/js/index.js
+++ #src/js/index.js
@@ -1,6 +1,17 @@
+//USER-BUTTON =========================================================
 
 let user_icon = document.querySelector(".user-header__icon")
 user_icon.addEventListener("click", (e) => {
   let user_menu = document.querySelector(".user-header__menu")
   user_menu.classList.toggle("_active")
+  e.preventDefault()
 })
+
+
+//BURGER-BUTTON =========================================================
+
+let menuButton = document.querySelector('.icon-menu');
+menuButton.addEventListener('click', function (e) {
+    menuButton.classList.toggle('_active');
+    e.preventDefault();
+});
