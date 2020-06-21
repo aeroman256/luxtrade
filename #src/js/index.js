48bcf517cabc4e62622311fcc2764e24a2546785
aeroman256 <zakharov.dmitry.92@yandex.ru>
2020-06-22 00:47:57 +0300

    header complete:

--- #src/js/index.js
+++ #src/js/index.js
@@ -7,11 +7,20 @@ user_icon.addEventListener("click", (e) => {
   e.preventDefault()
 })
 
+document.documentElement.addEventListener("click", function(e) {
+  if (!e.target.closest(".user-header")) {
+    let user_menu = document.querySelector(".user-header__menu")
+    user_menu.classList.remove("_active")
+  }
+})
+
 
 //BURGER-BUTTON =========================================================
 
 let menuButton = document.querySelector('.icon-menu');
+let menuBody = document.querySelector(".menu__body")
 menuButton.addEventListener('click', function (e) {
     menuButton.classList.toggle('_active');
+    menuBody.classList.toggle('_active')
     e.preventDefault();
 });
